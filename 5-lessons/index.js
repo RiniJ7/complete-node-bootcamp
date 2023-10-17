829bd55d6deb582a8adae73592d8b08b6b44b6c1
rinij <rinimani7@gmail.com>
2023-10-17 13:37:50 -0600

    write to a file and create a new file

--- 5-lessons/index.js
+++ 5-lessons/index.js
@@ -26,6 +26,9 @@ fs.readFile('./start.txt', 'utf-8',(err,data1)=> {
 		console.log(data2);
 		fs.readFile('./append.txt','utf-8', (err,data3) => {
 			console.log(data3);
+			fs.writeFile('./final.txt',`${data2} \n ${data3}`,'utf-8', err => {
+console.log("your file has been written :-)");
+			});
 		});
 	});
 });
