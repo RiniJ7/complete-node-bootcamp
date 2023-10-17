81c3c50f3fdadaed770ce825866da2047bb2b178
rinij <rinimani7@gmail.com>
2023-10-17 13:32:56 -0600

    cal back hell created

--- 5-lessons/index.js
+++ 5-lessons/index.js
@@ -24,6 +24,9 @@ const fs = require('fs');
 fs.readFile('./start.txt', 'utf-8',(err,data1)=> {
 	fs.readFile(`./${data1}.txt`,'utf-8', (err,data2)=> {
 		console.log(data2);
+		fs.readFile('./append.txt','utf-8', (err,data3) => {
+			console.log(data3);
+		});
 	});
 });
 
