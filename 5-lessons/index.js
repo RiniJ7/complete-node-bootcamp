1d827dca57dcaa3d57a12c65e3d64405758608cb
rinij <rinimani7@gmail.com>
2023-10-17 11:56:36 -0600

    .readFile configured

--- 5-lessons/index.js
+++ 5-lessons/index.js
@@ -11,11 +11,18 @@
 // Include fs module
 const fs = require('fs');
 
-// Calling the fs.readFileSync() method
-// for reading file 'input1.txt'
-const textIn = fs.readFileSync('./input.txt', 'utf8');
-console.log(textIn);
+// // Calling the fs.readFileSync() method
+// // for reading file 'input1.txt'
+// const textIn = fs.readFileSync('./input.txt', 'utf8');
+// console.log(textIn);
 
-const textOut = `This is what we know about avocado : ${textIn}.\n Created on ${Date.now()}`;
-fs.writeFileSync('./output.txt', textOut);
-console.log('file written!');
+// const textOut = `This is what we know about avocado : ${textIn}.\n Created on ${Date.now()}`;
+// fs.writeFileSync('./output.txt', textOut);
+// console.log('file written!');
+
+
+fs.readFile('./start.txt', 'utf-8',(err,data)=> {
+console.log(data);
+});
+
+console.log('will read file');
